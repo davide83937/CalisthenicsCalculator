d5de4dae243565acb2000bca1c599fab885946db
david <davidepanto@gmail.com>
2026-06-20 15:31:02 +0200

    seconda iterazione fatta

--- ITERAZIONE 1/Implementazione/SetLine.py
+++
@@ -1,7 +0,0 @@
-class setLine:
-    def __init__(self, skill, malus):
-        self.skill = skill
-        self.malus = malus
-
-    def __repr__(self):
-        return f"setLine(skill={self.skill!r}, malus={self.malus!r})"
